5bfdd4ba3983d86fc7f9b76007c4d7ac4695d31a
Tugay <33989361+tugaysarici@users.noreply.github.com>
2020-05-31 15:59:04 +0300

    Create chapter4.java

---
+++ chapter4.java
@@ -0,0 +1,63 @@
+// MATEMATİKSEL FONKSİYONLAR ve sabitler "Math" classı ile beraber kullanılır.
+// Trigonometrik Fonksiyonlar
+
+Math.sin(double a)
+Math.cos(double a)
+Math.tan(double a)
+Math.acos(double a)
+Math.asin(double a)
+Math.atan(double a)
+// Sabitler
+Math.PI
+Math.E
+
+// Örneğin; Math.sin(Math.PI / 6) ifadesi 0.5 döndürür.
+
+// Exponent (e üssü) Fonksiyonu
+Math.exp(double a)
+
+// ln Fonksiyonu
+Math.log(double a)
+
+// log Fonksiyonu
+Math.log10(double a)
+
+// Üst Alma Fonksiyonu (a üzeri b)
+Math.pow(double a, double b)
+
+// Karekök Alma Fonksionu
+Math.sqrt(double a)
+
+// Yuvarlama Fonksiyonları
+// Üstteki en yakın tam sayıya
+Math.ceil(double x)
+
+// Alttaki en yakın tam sayıya
+Math.floor(double x)
+
+// En yakın tam sayıya (2.5'de 2'ye, 2.51'de 3'e yuvarladı)
+Math.rint(double x)
+
+// 0.5 eklenmiş hallerini alttaki en yakın tamsayıya
+Math.round(double x)
+
+// Minimum değer bulma
+Math.min(2.5, 3.6)
+
+// Maximum değer bulma
+Math.max(2, 3)
+
+// Mutlak değer bulma
+Math.abs(-3.4)
+
+// [0.0, 1.0) aralığında rastgele bir double sayı
+Math.random()
+// ------------------------------------------------------------------------------------
+
+// Karakter Veri Tipi
+
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
