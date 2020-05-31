241dd2127e030bebb5088d854332439ad327a7aa
Tugay <33989361+tugaysarici@users.noreply.github.com>
2020-05-31 15:10:10 +0300

    Update chapter3.java

--- chapter3.java
+++ chapter3.java
@@ -3,8 +3,6 @@ boolean b = (1 > 2); // false döndürür
 
 // İLİŞKİSEL OPERATÖRLER
 //< <= > >= == !=   operatörlerine göre true ya da false
-
-import java.util.Scanner;
 public class AdditionQuiz {
   public static void main(String[] args) {
     int number1 = (int)(System.currentTimeMillis() % 10);
@@ -20,8 +18,6 @@ public class AdditionQuiz {
 // ------------------------------------------------------------------------------------
 // KOŞULLAR (CONDITIONS)
 // if, else if, else ile kurulur.
-
-import java.util.Scanner; 
 public class SubtractionQuiz {
   public static void main(String[] args) {
     // İki tane rastgele sayı üretir 1-10 arası
@@ -54,7 +50,74 @@ public class SubtractionQuiz {
 %% is and
 || is or
 ^ is exclusive or (aynı ise false, farklı ise true)
+
+public class TestBooleanOperators {
+  public static void main(String[] args) {
+    // Create a Scanner
+    Scanner input = new Scanner(System.in);
+
+    // Receive an input
+    System.out.print("Enter an integer: ");
+    int number = input.nextInt();
+
+    if (number % 2 == 0 && number % 3 == 0)
+      System.out.println(number + " is divisible by 2 and 3.");
+
+    if (number % 2 == 0 || number % 3 == 0)
+      System.out.println(number + " is divisible by 2 or 3.");
+
+    if (number % 2 == 0 ^ number % 3 == 0)
+      System.out.println(number + 
+        " divisible by 2 or 3, but not both.");
+  }
+}
 // ------------------------------------------------------------------------------------
+// Switch Kullanımı
+// Case'ler içerisinde break kullanılmazsa diğer case'ler de çalıştırılır.
+// default isteğe bağlı kullanılır. Case'lere girilmezse çalışır.
+// Switch içerisindeki değişken türü, case'dekilerle aynı olmalı.
+public class ChineseZodiac {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a year: ");
+    int year = input.nextInt();
+    
+    switch (year % 12) {
+      case 0: System.out.println("monkey"); break;
+      case 1: System.out.println("rooster"); break;
+      case 2: System.out.println("dog"); break;
+      case 3: System.out.println("pig"); break;
+      case 4: System.out.println("rat"); break;
+      case 5: System.out.println("ox"); break;
+      case 6: System.out.println("tiger"); break;
+      case 7: System.out.println("rabbit"); break;
+      case 8: System.out.println("dragon"); break;
+      case 9: System.out.println("snake"); break;
+      case 10: System.out.println("horse"); break;
+      case 11: System.out.println("sheep"); break;
+      default: System.out.println("invalid"); System.exit(1);
+    }
+  }
+}
+  
+// Koşulsal İfadelendirme (Conditional Expression)
+// (boolean-expression) ? expression1 : expression2
+// boolean-expression true ise expression1, false ise expression2 çalışır.
+System.out.println((num % 2 == 0)? num + “is even” : num + “is odd”);
 // ------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------
+
+// İşlem Öncelik Sırası (Operator Precedence)
+ ()
+ var++, var--
+ +, - (Unary plus and minus), ++var,--var
+ (type) Casting
+ ! (Not)
+ *, /, % (Multiplication, division, and remainder)
+ +, - (Binary addition and subtraction)
+ <, <=, >, >= (Relational operators)
+ ==, !=; (Equality)
+ ^ (Exclusive OR)
+ && (Conditional AND) Short-circuit AND
+ || (Conditional OR) Short-circuit OR
+ =, +=, -=, *=, /=, %= (Assignment operator)
 // ------------------------------------------------------------------------------------
